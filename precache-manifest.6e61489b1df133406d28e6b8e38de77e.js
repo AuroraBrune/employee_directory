7f769976d83c8f1ea3fb1c3a6fce5592a3e03b40
Aurora Brune <akbrune@hotmail.com>
2020-10-24 23:22:40 -0500

    Updates

---
+++ precache-manifest.6e61489b1df133406d28e6b8e38de77e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aade6c0fb8fc2f52bdc714229c65092",
+    "url": "/employee_directory/index.html"
+  },
+  {
+    "revision": "8e51f37b5d0594894e74",
+    "url": "/employee_directory/static/css/main.c4b5f324.chunk.css"
+  },
+  {
+    "revision": "f16c7bc70498fe4d5cce",
+    "url": "/employee_directory/static/js/2.39a994bc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/employee_directory/static/js/2.39a994bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e51f37b5d0594894e74",
+    "url": "/employee_directory/static/js/main.cef6bf0a.chunk.js"
+  },
+  {
+    "revision": "e27f1e9c9d80bee3572c",
+    "url": "/employee_directory/static/js/runtime-main.bba967ae.js"
+  }
+]);
