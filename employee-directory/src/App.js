22e5c82fcf665f4ad6acb8b3fdb96d758a5a0588
Aurora Brune <akbrune@hotmail.com>
2020-10-24 21:07:30 -0500

    app.js file

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import EmployeesTable from './components/EmployeesTable';
 import Wrapper from './components/Wrapper';
 import employees from './employees.json';
-import DataTable from 'react-data-table-component';
 
 class App extends Component {
   state = {
@@ -12,8 +10,9 @@ class App extends Component {
 
   render() {
     return (
-      <Wrapper />
-
+      <div>
+        <Wrapper />
+      </div>
     );
   }
 }
